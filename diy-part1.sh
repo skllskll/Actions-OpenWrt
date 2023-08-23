54289d0905dfd7c8ceefda672ed8dec09b49d007
skllskll <skllmirk@gmail.com>
2023-08-23 14:14:26 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -15,5 +15,5 @@
 
 # Add a feed source
 echo 'src-git helloworld https://github.com/fw876/helloworld' >>feeds.conf.default
-echo 'src-git PWpackages https://github.com/xiaorouji/openwrt-passwall.git;packages' >> feeds.conf.default
-echo 'src-git PWluci https://github.com/xiaorouji/openwrt-passwall.git;luci' >> feeds.conf.default
+#echo 'src-git PWpackages https://github.com/xiaorouji/openwrt-passwall.git;packages' >> feeds.conf.default
+#echo 'src-git PWluci https://github.com/xiaorouji/openwrt-passwall.git;luci' >> feeds.conf.default
